41de14d71cf4198901c0089f8189e869d8501487
donbas777 <renary34@gmail.com>
2024-05-14 17:31:37 +0300

    Update settings and requirements.txt

--- NewspaperAgency/settings.py
+++ NewspaperAgency/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gw_rpc=3f(upu3_frbee%#p_!-1xaz806a$pe@^v&ow-x2(xa&'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-gw_rpc=3f(upu3_frbee%#p_!-1xaz806a$pe@^v&ow-x2(xa&")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
--- NewspaperAgency/urls.py
+++ NewspaperAgency/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("agency.urls", namespace="agensy")),
+    path("", include("agency.urls", namespace="agency")),
     path("accounts/", include("django.contrib.auth.urls")),
     path("__debug__/", include("debug_toolbar.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
